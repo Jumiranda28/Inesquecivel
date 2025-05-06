bb10d8ad031539573b2a2ebb6ea5f11ead141bcc
Jumiranda28 <dri.kane@hotmail.com>
2025-05-06 16:51:59 -0300

    ajustando o token junto com o bearer

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,5 +5,12 @@ export function authMiddleware(request, response, next) {
     //capturando o token da requisição
     const authMiddleware = request.headers.authorization;
 
+    if (!authMiddleware|| !authMiddleware.startsWith("Bearer ")) {
+        return response.status(401).json({
+            msg: "Token não encontrado"
+        });
+    }
+    
+    const [bearer, token] = authMiddleware.split(" ");
 
 }
